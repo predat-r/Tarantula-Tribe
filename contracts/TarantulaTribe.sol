9d83f93f57de1b4c6f6ef05b15c0eb434e4a4a03
predat-r <harisnaeem612@outlook.com>
2025-02-17 21:47:41 +0500

    ipfs url, adjusted contract for minting all to owner and set royalty fees

--- contracts/TarantulaTribe.sol
+++ contracts/TarantulaTribe.sol
@@ -2,20 +2,20 @@
 pragma solidity ^0.8.17;
 
 import "erc721a/contracts/ERC721A.sol";
-
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-contract TarantulaTribe is ERC721A , Ownable
- {
-
-    string private baseURI;  
-    uint256 public constant MAX_SUPPLY = 25;  
+contract TarantulaTribe is ERC721A, Ownable, IERC2981 {
+    uint256 public constant MAX_SUPPLY = 25;
+    uint96 public constant ROYALTY_FEE = 500; // 5% royalty fee (500 basis points)
+    string private baseURI;
+    address private royaltyRecipient;
 
     constructor(string memory initialBaseURI) ERC721A("Tarantula Tribe", "TARANTULA") Ownable(msg.sender) {
         baseURI = initialBaseURI; 
-        _mint(msg.sender, 1);  // Minting 1 NFT to deployer 
+        royaltyRecipient = owner();
+        _mint(msg.sender, MAX_SUPPLY); // Mint all NFTs to the owner
     }
-
 
     // Returns the base URI for metadata stored on IPFS
     function _baseURI() internal view override returns (string memory) {
@@ -25,5 +25,22 @@ contract TarantulaTribe is ERC721A , Ownable
     // Allows owner to update metadata location 
     function setBaseURI(string memory _newBaseURI) external onlyOwner {
         baseURI = _newBaseURI;
+    }
+
+    // Update royalty recipient
+    function setRoyaltyRecipient(address _newRecipient) external onlyOwner {
+        require(_newRecipient != address(0), "Invalid address");
+        royaltyRecipient = _newRecipient;
+    }
+
+    // ERC-2981 royalty info function
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice) external view override returns (address receiver, uint256 royaltyAmount) {
+        require(_exists(_tokenId), "Token does not exist");
+        return (royaltyRecipient, (_salePrice * ROYALTY_FEE) / 10000);
+    }
+
+    // Support interface check for ERC-2981
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, IERC165) returns (bool) {
+        return interfaceId == type(IERC2981).interfaceId || super.supportsInterface(interfaceId);
     }
 }
--- ignition/modules/TarantulaTribe.ts
+++ ignition/modules/TarantulaTribe.ts
@@ -1,7 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const TarantulaTribeModule = buildModule("TarantulaTribeModule", (m) => {
-  const baseURI = m.getParameter("baseURI", "ipfs://your-default-uri/");
+  const baseURI = m.getParameter(
+    "baseURI", 
+    "ipfs://bafybeia26ygohk43wpfwuolm2qjtxg5ot2fmlwtstc3szfrjmqxtdxj2u4/"
+  );
 
   const tarantulaTribe = m.contract("TarantulaTribe", [baseURI]);
 
